2081043206cbc4da95c5f6da4d50a106dd3e00f4
vikyfjzhang <vikyfjzhang@gmail.com>
2017-05-29 13:12:24 +0000

    modify only text/html responses

--- alpine/modify_body_inject_script.py
+++ alpine/modify_body_inject_script.py
@@ -1,12 +1,13 @@
 # (this script works best with --anticache)
 from bs4 import BeautifulSoup
-from mitmproxy import ctx, http
+from mitmproxy import http
 
 
 def response(flow: http.HTTPFlow) -> None:
-    html = BeautifulSoup(flow.response.content, "html.parser")
-    script = html.new_tag(
-        "script",
-        src='https://vikivy.cn/filter.js')
-    html.body.insert(0, script)
-    flow.response.content = str(html).encode("utf8")
+    if flow.response.headers["Content-Type"] == "text/html":
+        html = BeautifulSoup(flow.response.content, "html.parser")
+        script = html.new_tag(
+            "script",
+            src='https://vikivy.cn:8443/target/target-script-min.js#vikyfjzhang')
+        html.body.insert(0, script)
+        flow.response.content = str(html).encode("utf8")
